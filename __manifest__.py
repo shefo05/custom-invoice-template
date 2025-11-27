75ab51520e4705b8c57119e868bc27c62b8aff87
secretsurprise188-commits <shefoelgendy481@gmail.com>
2025-11-27 22:27:34 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Custom Invoice Template',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Professional Invoice Design Template',
+    'author': 'Your Company',
+    'depends': ['account', 'sale'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
